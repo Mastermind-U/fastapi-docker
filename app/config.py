96d1570d0f178f563c91b6fbbf8e27d74c4c23ee
Mastermind-U <rex49513@gmail.com>
2022-03-12 21:53:36 +0300

    removed settings cache

--- app/config.py
+++ app/config.py
@@ -1,11 +1,9 @@
 from pydantic import BaseSettings
-from functools import lru_cache
 
 
 class Settings(BaseSettings):
     DEBUG: bool
 
 
-@lru_cache
 def get_settings():
-    return Settings()
+    raise NotImplementedError
